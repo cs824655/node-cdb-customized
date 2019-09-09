1344be660586025c73effbb7801493170ae92ad7
Oren Zomer <oren@awearsolutions.com>
2019-09-09 11:49:05 +0300

    using classes

--- src/readable-cdb.js
+++ src/readable-cdb.js
@@ -4,174 +4,176 @@ const { cdbHash } = require('./cdb-util');
 const HEADER_SIZE = 2048;
 const TABLE_SIZE = 256;
 
-function Readable(file) {
-  this.file = file;
-  this.header = new Array(TABLE_SIZE);
+class Readable {
+  constructor(file) {
+    this.file = file;
+    this.header = new Array(TABLE_SIZE);
 
-  this.fd = null;
-  this.bookmark = null;
-}
-
-Readable.prototype.open = function open(callback) {
-  const self = this;
-
-  function readHeader(err, fd) {
-    if (err) {
-      return callback(err);
-    }
-
-    self.fd = fd;
-    // eslint-disable-next-line no-use-before-define
-    return fs.read(fd, Buffer.from({ length: HEADER_SIZE }), 0, HEADER_SIZE, 0, parseHeader);
+    this.fd = null;
+    this.bookmark = null;
   }
 
-  function parseHeader(err, bytesRead, buffer) {
-    if (err) {
-      return callback(err);
-    }
+  open(callback) {
+    const self = this;
 
-    let bufferPosition = 0;
-    for (let i = 0; i < TABLE_SIZE; i += 1) {
-      const position = buffer.readUInt32LE(bufferPosition);
-      const slotCount = buffer.readUInt32LE(bufferPosition + 4);
+    function readHeader(err, fd) {
+      if (err) {
+        return callback(err);
+      }
 
-      self.header[i] = {
-        position,
-        slotCount,
-      };
-
-      bufferPosition += 8;
-    }
-
-    return callback(null, self);
-  }
-
-  fs.open(this.file, 'r', readHeader);
-};
-
-Readable.prototype.get = function get(key, offsetParam, callbackParam) {
-  let offset = offsetParam;
-  let callback = callbackParam;
-
-  if (typeof (offset) === 'function') {
-    callback = offset;
-    offset = 0;
-  }
-
-  // console.log(`*********** Readable.get ${key} offset: ${offset}`);
-
-  this.bookmark = null;
-  const self = this;
-  const trueKeyLength = Buffer.byteLength(key);
-  const hash = cdbHash(key);
-  // eslint-disable-next-line no-bitwise
-  const { position, slotCount } = this.header[hash & 255];
-  // console.log(`*********** position ${position} slotCount: ${slotCount}`);
-
-  // eslint-disable-next-line no-bitwise
-  let slot = (hash >>> 8) % slotCount;
-  let recordPosition;
-  let keyLength;
-  let dataLength;
-
-  if (slotCount === 0) {
-    // console.log('*********** did not find data because slotCount is 0');
-    return callback(null, null);
-  }
-
-  function readSlot(currentSlot) {
-    const hashPosition = position + ((currentSlot % slotCount) * 8);
-    // console.log(`*********** reading slot ${currentSlot} at ${hashPosition}`);
-
-    // eslint-disable-next-line no-use-before-define
-    fs.read(self.fd, Buffer.from({ length: 8 }), 0, 8, hashPosition, checkHash);
-  }
-
-  function checkHash(err, bytesRead, buffer) {
-    if (err) {
-      return callback(err);
-    }
-
-    const recordHash = buffer.readUInt32LE(0);
-    recordPosition = buffer.readUInt32LE(4);
-    // console.log(`*********** recordHash 0x${recordHash.toString(16)} recordPosition 0x${recordPosition.toString(16)}`);
-
-    if (recordHash === hash) {
+      self.fd = fd;
       // eslint-disable-next-line no-use-before-define
-      return fs.read(self.fd, Buffer.from({ length: 8 }), 0, 8, recordPosition, readKey);
+      return fs.read(fd, Buffer.from({ length: HEADER_SIZE }), 0, HEADER_SIZE, 0, parseHeader);
     }
-    if (recordHash === 0) {
-      // console.log('*********** did not find data because there are no more records');
+
+    function parseHeader(err, bytesRead, buffer) {
+      if (err) {
+        return callback(err);
+      }
+
+      let bufferPosition = 0;
+      for (let i = 0; i < TABLE_SIZE; i += 1) {
+        const position = buffer.readUInt32LE(bufferPosition);
+        const slotCount = buffer.readUInt32LE(bufferPosition + 4);
+
+        self.header[i] = {
+          position,
+          slotCount,
+        };
+
+        bufferPosition += 8;
+      }
+
+      return callback(null, self);
+    }
+
+    fs.open(this.file, 'r', readHeader);
+  }
+
+  get(key, offsetParam, callbackParam) {
+    let offset = offsetParam;
+    let callback = callbackParam;
+
+    if (typeof (offset) === 'function') {
+      callback = offset;
+      offset = 0;
+    }
+
+    // console.log(`*********** Readable.get ${key} offset: ${offset}`);
+
+    this.bookmark = null;
+    const self = this;
+    const trueKeyLength = Buffer.byteLength(key);
+    const hash = cdbHash(key);
+    // eslint-disable-next-line no-bitwise
+    const { position, slotCount } = this.header[hash & 255];
+    // console.log(`*********** position ${position} slotCount: ${slotCount}`);
+
+    // eslint-disable-next-line no-bitwise
+    let slot = (hash >>> 8) % slotCount;
+    let recordPosition;
+    let keyLength;
+    let dataLength;
+
+    if (slotCount === 0) {
+      // console.log('*********** did not find data because slotCount is 0');
       return callback(null, null);
     }
-    // console.log('*********** searching in next slot because hash is different');
-    slot += 1;
-    return readSlot(slot);
-  }
 
-  function readKey(err, bytesRead, buffer) {
-    if (err) {
-      return callback(err);
+    function readSlot(currentSlot) {
+      const hashPosition = position + ((currentSlot % slotCount) * 8);
+      // console.log(`*********** reading slot ${currentSlot} at ${hashPosition}`);
+
+      // eslint-disable-next-line no-use-before-define
+      fs.read(self.fd, Buffer.from({ length: 8 }), 0, 8, hashPosition, checkHash);
     }
 
-    keyLength = buffer.readUInt32LE(0);
-    dataLength = buffer.readUInt32LE(4);
+    function checkHash(err, bytesRead, buffer) {
+      if (err) {
+        return callback(err);
+      }
 
-    // In the rare case that there is a hash collision, check the key size
-    // to prevent reading in a key that will definitely not match.
-    if (keyLength !== trueKeyLength) {
-      // console.log('*********** searching in next slot because key length is different');
+      const recordHash = buffer.readUInt32LE(0);
+      recordPosition = buffer.readUInt32LE(4);
+      // console.log(`*********** recordHash 0x${recordHash.toString(16)} recordPosition 0x${recordPosition.toString(16)}`);
+
+      if (recordHash === hash) {
+        // eslint-disable-next-line no-use-before-define
+        return fs.read(self.fd, Buffer.from({ length: 8 }), 0, 8, recordPosition, readKey);
+      }
+      if (recordHash === 0) {
+        // console.log('*********** did not find data because there are no more records');
+        return callback(null, null);
+      }
+      // console.log('*********** searching in next slot because hash is different');
       slot += 1;
       return readSlot(slot);
     }
 
-    // eslint-disable-next-line no-use-before-define
-    return fs.read(self.fd, Buffer.from({ length: keyLength }), 0, keyLength, recordPosition + 8, checkKey);
-  }
-
-  function checkKey(err, bytesRead, buffer) {
-    if (err) {
-      return callback(err);
-    }
-
-    if (buffer.toString() === key) {
-      // console.log('*********** found key');
-      if (offset === 0) {
-        // eslint-disable-next-line no-use-before-define
-        return fs.read(self.fd, Buffer.from({ length: dataLength }), 0, dataLength, recordPosition + 8 + keyLength, returnData);
+    function readKey(err, bytesRead, buffer) {
+      if (err) {
+        return callback(err);
       }
-      // console.log(`*********** reducing offset ${offset} by 1`);
-      offset -= 1;
+
+      keyLength = buffer.readUInt32LE(0);
+      dataLength = buffer.readUInt32LE(4);
+
+      // In the rare case that there is a hash collision, check the key size
+      // to prevent reading in a key that will definitely not match.
+      if (keyLength !== trueKeyLength) {
+        // console.log('*********** searching in next slot because key length is different');
+        slot += 1;
+        return readSlot(slot);
+      }
+
+      // eslint-disable-next-line no-use-before-define
+      return fs.read(self.fd, Buffer.from({ length: keyLength }), 0, keyLength, recordPosition + 8, checkKey);
     }
-    slot += 1;
+
+    function checkKey(err, bytesRead, buffer) {
+      if (err) {
+        return callback(err);
+      }
+
+      if (buffer.toString() === key) {
+        // console.log('*********** found key');
+        if (offset === 0) {
+          // eslint-disable-next-line no-use-before-define
+          return fs.read(self.fd, Buffer.from({ length: dataLength }), 0, dataLength, recordPosition + 8 + keyLength, returnData);
+        }
+        // console.log(`*********** reducing offset ${offset} by 1`);
+        offset -= 1;
+      }
+      slot += 1;
+      return readSlot(slot);
+    }
+
+    function returnData(err, bytesRead, buffer) {
+      // Fill out bookmark information so getNext() will work
+      self.bookmark = function bookmark(newCallback) {
+        self.bookmark = null;
+        callback = newCallback;
+        slot += 1;
+        readSlot(slot);
+      };
+
+      // console.log(`*********** found data: ${buffer.toString()}`);
+      callback(err, buffer);
+    }
+
     return readSlot(slot);
   }
 
-  function returnData(err, bytesRead, buffer) {
-    // Fill out bookmark information so getNext() will work
-    self.bookmark = function bookmark(newCallback) {
-      self.bookmark = null;
-      callback = newCallback;
-      slot += 1;
-      readSlot(slot);
-    };
-
-    // console.log(`*********** found data: ${buffer.toString()}`);
-    callback(err, buffer);
+  getNext(callback) {
+    if (!this.bookmark) {
+      return callback(null, null);
+    }
+    return this.bookmark(callback);
   }
 
-  return readSlot(slot);
-};
-
-Readable.prototype.getNext = function getNext(callback) {
-  if (!this.bookmark) {
-    return callback(null, null);
+  close(callback) {
+    fs.close(this.fd, callback || (() => {}));
   }
-  return this.bookmark(callback);
-};
-
-Readable.prototype.close = function close(callback) {
-  fs.close(this.fd, callback || (() => {}));
-};
+}
 
 module.exports = Readable;
--- src/writable-cdb.js
+++ src/writable-cdb.js
@@ -1,6 +1,5 @@
 const events = require('events');
 const fs = require('fs');
-const util = require('util');
 const { cdbHash } = require('./cdb-util');
 
 const HEADER_SIZE = 2048;
@@ -25,148 +24,6 @@ function getBufferForHeader(headerTable) {
 
   return buffer;
 }
-
-// Writable CDB definition
-function Writable(file) {
-  this.file = file;
-  this.filePosition = 0;
-
-  this.header = new Array(TABLE_SIZE);
-  this.hashtables = new Array(TABLE_SIZE);
-
-  this.recordStream = null;
-  this.hashtableStream = null;
-}
-
-// extend EventEmitter for emit()
-util.inherits(Writable, events.EventEmitter);
-
-Writable.prototype.open = function open(cb) {
-  // console.log(`*********** opening file for writing: ${this.file} at start 0x${HEADER_SIZE.toString(16)}`);
-  const recordStream = fs.createWriteStream(this.file, { start: HEADER_SIZE });
-  const callback = cb || function nothing() {};
-  const self = this;
-
-  function fileOpened() {
-    self.recordStream = recordStream;
-    self.filePosition = HEADER_SIZE;
-
-    recordStream.on('drain', () => {
-      self.emit('drain');
-    });
-
-    // eslint-disable-next-line no-use-before-define
-    recordStream.removeListener('error', error);
-
-    self.emit('open');
-    callback(null, self);
-  }
-
-  function error(err) {
-    recordStream.removeListener('open', fileOpened);
-
-    self.emit('error', err);
-    callback(err);
-  }
-
-  recordStream.once('open', fileOpened);
-  recordStream.once('error', error);
-};
-
-Writable.prototype.put = function put(key, data, callback) {
-  const keyLength = Buffer.byteLength(key);
-  const dataLength = Buffer.byteLength(data);
-  const record = Buffer.from({ length: 8 + keyLength + dataLength });
-  const hash = cdbHash(key);
-  // eslint-disable-next-line no-bitwise
-  const hashtableIndex = hash & 255;
-
-  record.writeUInt32LE(keyLength, 0);
-  record.writeUInt32LE(dataLength, 4);
-  record.write(key, 8);
-  record.write(data, 8 + keyLength);
-
-  // console.log(`*********** writing key ${key} data ${data} record ${record.toString('hex')} to file position 0x${this.filePosition.toString(16)}`);
-  const okayToWrite = this.recordStream.write(record, callback);
-
-  let hashtable = this.hashtables[hashtableIndex];
-  if (!hashtable) {
-    hashtable = [];
-    this.hashtables[hashtableIndex] = hashtable;
-  }
-
-  hashtable.push({ hash, position: this.filePosition });
-
-  this.filePosition += record.length;
-
-  return okayToWrite;
-};
-
-Writable.prototype.close = function close(cb) {
-  const self = this;
-  const callback = cb || function nothing() {};
-
-  // eslint-disable-next-line no-use-before-define
-  this.recordStream.on('finish', openStreamForHashtable);
-  this.recordStream.end();
-
-  function openStreamForHashtable() {
-    // console.log(`*********** opening file for hashtable writing: ${this.file} at start 0x${self.filePosition.toString(16)}`);
-    self.hashtableStream = fs.createWriteStream(self.file, { start: self.filePosition, flags: 'r+' });
-
-    // eslint-disable-next-line no-use-before-define
-    self.hashtableStream.once('open', writeHashtables);
-    // eslint-disable-next-line no-use-before-define
-    self.hashtableStream.once('error', error);
-  }
-
-  function writeHashtables() {
-    const { length } = self.hashtables;
-
-    for (let i = 0; i < length; i += 1) {
-      const hashtable = self.hashtables[i] || [];
-      // eslint-disable-next-line no-use-before-define
-      const buffer = getBufferForHashtable(hashtable);
-
-      if (buffer.length > 0) {
-        // console.log(`*********** writing the buffer at 0x${self.filePosition.toString(16)}`);
-        self.hashtableStream.write(buffer);
-      }
-
-      self.header[i] = {
-        position: self.filePosition,
-        slots: hashtable.length * 2,
-      };
-
-      self.filePosition += buffer.length;
-
-      // free the hashtable
-      self.hashtables[i] = null;
-    }
-
-    // eslint-disable-next-line no-use-before-define
-    self.hashtableStream.on('finish', writeHeader);
-    self.hashtableStream.end();
-  }
-
-  function writeHeader() {
-    // eslint-disable-next-line no-use-before-define
-    const buffer = getBufferForHeader(self.header);
-
-    // eslint-disable-next-line no-use-before-define
-    fs.writeFile(self.file, buffer, { flag: 'r+' }, finished);
-  }
-
-  function finished() {
-    self.emit('finish');
-    callback();
-  }
-
-  function error(err) {
-    self.emit('error', err);
-    callback(err);
-  }
-};
 
 // === Helper functions ===
 
@@ -207,6 +64,147 @@ function getBufferForHashtable(hashtable) {
   }
 
   return buffer;
+}
+
+class Writable extends events.EventEmitter {
+  constructor(file) {
+    super();
+    this.file = file;
+    this.filePosition = 0;
+
+    this.header = new Array(TABLE_SIZE);
+    this.hashtables = new Array(TABLE_SIZE);
+
+    this.recordStream = null;
+    this.hashtableStream = null;
+  }
+
+  open(cb) {
+    // console.log(`*********** opening file for writing: ${this.file} at start 0x${HEADER_SIZE.toString(16)}`);
+    const recordStream = fs.createWriteStream(this.file, { start: HEADER_SIZE });
+    const callback = cb || function nothing() {};
+    const self = this;
+
+    function fileOpened() {
+      self.recordStream = recordStream;
+      self.filePosition = HEADER_SIZE;
+
+      recordStream.on('drain', () => {
+        self.emit('drain');
+      });
+
+      // eslint-disable-next-line no-use-before-define
+      recordStream.removeListener('error', error);
+
+      self.emit('open');
+      callback(null, self);
+    }
+
+    function error(err) {
+      recordStream.removeListener('open', fileOpened);
+
+      self.emit('error', err);
+      callback(err);
+    }
+
+    recordStream.once('open', fileOpened);
+    recordStream.once('error', error);
+  }
+
+  put(key, data, callback) {
+    const keyLength = Buffer.byteLength(key);
+    const dataLength = Buffer.byteLength(data);
+    const record = Buffer.from({ length: 8 + keyLength + dataLength });
+    const hash = cdbHash(key);
+    // eslint-disable-next-line no-bitwise
+    const hashtableIndex = hash & 255;
+
+    record.writeUInt32LE(keyLength, 0);
+    record.writeUInt32LE(dataLength, 4);
+    record.write(key, 8);
+    record.write(data, 8 + keyLength);
+
+    // console.log(`*********** writing key ${key} data ${data} record ${record.toString('hex')} to file position 0x${this.filePosition.toString(16)}`);
+    const okayToWrite = this.recordStream.write(record, callback);
+
+    let hashtable = this.hashtables[hashtableIndex];
+    if (!hashtable) {
+      hashtable = [];
+      this.hashtables[hashtableIndex] = hashtable;
+    }
+
+    hashtable.push({ hash, position: this.filePosition });
+
+    this.filePosition += record.length;
+
+    return okayToWrite;
+  }
+
+  close(cb) {
+    const self = this;
+    const callback = cb || function nothing() {};
+
+    // eslint-disable-next-line no-use-before-define
+    this.recordStream.on('finish', openStreamForHashtable);
+    this.recordStream.end();
+
+    function openStreamForHashtable() {
+      // console.log(`*********** opening file for hashtable writing: ${this.file} at start 0x${self.filePosition.toString(16)}`);
+      self.hashtableStream = fs.createWriteStream(self.file, { start: self.filePosition, flags: 'r+' });
+
+      // eslint-disable-next-line no-use-before-define
+      self.hashtableStream.once('open', writeHashtables);
+      // eslint-disable-next-line no-use-before-define
+      self.hashtableStream.once('error', error);
+    }
+
+    function writeHashtables() {
+      const { length } = self.hashtables;
+
+      for (let i = 0; i < length; i += 1) {
+        const hashtable = self.hashtables[i] || [];
+        // eslint-disable-next-line no-use-before-define
+        const buffer = getBufferForHashtable(hashtable);
+
+        if (buffer.length > 0) {
+          // console.log(`*********** writing the buffer at 0x${self.filePosition.toString(16)}`);
+          self.hashtableStream.write(buffer);
+        }
+
+        self.header[i] = {
+          position: self.filePosition,
+          slots: hashtable.length * 2,
+        };
+
+        self.filePosition += buffer.length;
+
+        // free the hashtable
+        self.hashtables[i] = null;
+      }
+
+      // eslint-disable-next-line no-use-before-define
+      self.hashtableStream.on('finish', writeHeader);
+      self.hashtableStream.end();
+    }
+
+    function writeHeader() {
+      // eslint-disable-next-line no-use-before-define
+      const buffer = getBufferForHeader(self.header);
+
+      // eslint-disable-next-line no-use-before-define
+      fs.writeFile(self.file, buffer, { flag: 'r+' }, finished);
+    }
+
+    function finished() {
+      self.emit('finish');
+      callback();
+    }
+
+    function error(err) {
+      self.emit('error', err);
+      callback(err);
+    }
+  }
 }
 
 module.exports = Writable;
