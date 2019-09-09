a6aab2e72a46ff4123f33a496e89f4da02f4011a
Oren Zomer <oren@awearsolutions.com>
2019-09-09 21:48:13 +0300

    readable and writable

--- index.js
+++ index.js
@@ -1,9 +1,2 @@
-'use strict';
-
-var writable = require('./src/writable-cdb'),
-    readable = require('./src/readable-cdb');
-
-module.exports = {
-    writable: writable,
-    readable: readable
-};
+exports.Writable = require('./src/writable-cdb');
+exports.Readable = require('./src/readable-cdb');
